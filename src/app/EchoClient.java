f7e2312c0d8d202a2b48062c3ed68bc93168eee0
brandonnhem <brandonnhem@gmail.com>
2020-04-24 02:25:02 -0700

    Working code

--- src/app/EchoClient.java
+++ src/app/EchoClient.java
@@ -1,5 +1,35 @@
 package app;
 
-public class EchoClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        if(args.length == 0) {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Enter IP address: ");
+            String ip = keyboard.nextLine();
+            System.out.println();
+            System.out.print("Enter Port Number: ");
+            int portNumber = keyboard.nextInt();
+            System.out.println();
+            socket = new Socket(ip, portNumber);
+        } else {
+            socket = new Socket(args[0], Integer.parseInt(args[1]));
+        }
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        while((str = input.readLine()) != null) {
+            out.println(str);
+            System.out.println("Echo: " + str);
+        }
+    }
 }
--- src/app/EchoServer.java
+++ src/app/EchoServer.java
@@ -1,5 +1,28 @@
 package app;
 
-public class EchoServer {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class EchoServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(6789);
+        System.out.println("Server started on port number 6789");
+        System.out.println("Awaiting connection...");
+
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected successfully");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        String inputLine;
+        while((inputLine = in.readLine()) != null) {
+            System.out.println("Server: " + inputLine);
+            out.println(inputLine);
+        }
+        System.out.println("Ending connection with client");
+    }
 }
